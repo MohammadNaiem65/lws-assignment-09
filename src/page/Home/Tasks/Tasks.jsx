a8449a08c2354c10020bf10171f2284e2832bc17
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-10-25 11:55:07 +0600

    made the tasks section dynamic

--- src/page/Home/Tasks/Tasks.jsx
+++ src/page/Home/Tasks/Tasks.jsx
@@ -1,11 +1,37 @@
+import { useGetTasksQuery } from '../../../features/tasks/tasksApi';
+import Loading from '../../../ui/Loading';
+import Error from '../../../ui/Error';
+import NoContent from '../../../ui/NoContent';
 import Task from '../Task/Task';
 
 export default function Tasks() {
-	return (
-		<div className='lws-task-list'>
-			<Task />
-			<Task />
-			<Task />
-		</div>
-	);
+	// hooks
+	const {
+		data: tasks,
+		isLoading,
+		isSuccess,
+		isError,
+		error,
+	} = useGetTasksQuery();
+
+	// decide what to render
+	let content;
+
+	if (isLoading) {
+		content = <Loading />;
+	} else if (!isLoading && isError) {
+		content = <Error message={error} />;
+	} else if (!isLoading && isSuccess && tasks.length === 0) {
+		content = <NoContent />;
+	} else if (!isLoading && isSuccess && tasks.length > 0) {
+		content = (
+			<>
+				{tasks.map((task) => (
+					<Task key={task.id} task={task} />
+				))}
+			</>
+		);
+	}
+
+	return <section className='lws-task-list'>{content}</section>;
 }
